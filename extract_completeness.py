cc486faed622bedfd67b770666878a197cb8579c
Thomas J. Creedy <thomas@tjcreedy.co.uk>
2023-06-02 16:32:21 +0100

    removed unneeded arguments, added length check, untested

--- extract_completeness.py
+++ extract_completeness.py
@@ -3,6 +3,7 @@
 # Imports
 
 import argparse
+
 import textwrap as _textwrap
 import urllib
 import os
@@ -65,6 +66,96 @@ def loadnamevariants(report=False):
     if not report:
         return output, alltypes, fullparse
 
+def parse_specs(nameconvert):
+    # Parse the mitocorrect master specifications file
+
+    specs = dict()
+
+    url = "https://raw.githubusercontent.com/tjcreedy/mitocorrect/bc42c7926947c0f35436d84d2ba7e76fd52d7cf5/specifications/mitocorrect_specifications_coleoptera_2022-02-24.tsv"
+    sh = urllib.request.urlopen(url)
+
+    # Extract the header row and split to list
+    header = sh.readline().strip().split('\t')
+
+    if len(set(header)) < len(header):
+        sys.exit(f"Error: duplicate headers in {url}")
+
+    ln = 1
+    for line in sh:
+        # line = sh.readline()
+
+        ln += 1
+
+        # Extract the values of the row and split to list
+        items = line.strip().split('\t')
+
+        # Get correct gene name
+        name = None
+        if items[0].upper() in nameconvert:
+            name = nameconvert[items[0].upper()]
+
+        if name is None:
+            sys.exit(f"Error: gene name {items[0]} in first column of line {ln} is not recognised")
+
+        # Generate holding dict for this line's specs
+        hold = dict()
+
+        # Work through specifications
+        for spec, value in zip(header[1:], items[1:]):
+            # spec, value = [header[2], items[2]]
+            # Do not record if value is empty
+            if value == '':
+                continue
+            # If a list specification, generate list
+            if '/' in value:
+                value = value.split('/')
+            # If a dict specification, generate dict
+            if (type(value) is list and ',' in value[0] or
+                    type(value) is str and ',' in value):
+
+                if type(value) is list:
+                    value = [v.split(',') for v in value]
+                else:
+                    value = [value.split(',')]
+
+                value = {k: v for k, v in value}
+
+            # Add to specs dict
+            hold[spec] = value
+
+        # Do input checking
+
+        for spec in hold.keys():
+            # spec = 'overlap'
+            value = hold[spec]
+            err = f"Error: value {value} for {spec} on line {ln}"
+
+            if spec == 'length':
+                # should be > 0 integer
+                if not str_is_int(value):
+                    sys.exit(f"{err} is not an integer")
+                value = int(value)
+                if value < 1:
+                    sys.exit(f"{err} is not greater than 0")
+                hold[spec] = value
+
+        del hold['end']
+        
+        # Initialise subdict if not already
+        if name not in specs:
+            specs[name] = dict()
+
+        # Extract length and percent variation
+        for i in ['length', 'lengthvariation']:
+            if i in hold:
+                specs[name][i] = hold.pop(i)
+
+    sh.close()
+
+    return specs
+
+
+
 
 def get_feat_name(feat):
     featname = "unknown"
@@ -95,40 +186,19 @@ This script finds and extracts sequences corresponding to CDS, rRNA and/or tRNA 
                         help="the path(s) to one or more genbank files from which gene sequences "
                              "should be extracted")
     parser.add_argument("-o", "--output", type=str, metavar='PATH', required=True,
-                        help="the path to a directory in which to place a fasta file for each "
-                             "gene, this will be created if it doesn't exist")
-    parser.add_argument("-m", "--mingenes", type=int, metavar='N', default=0,
-                        help="the minimum number of genes that must be present in an entry for "
-                             "any genes to be output")
-    parser.add_argument("-q", "--reqgenes", type=str, metavar='GENE', nargs='*',
-                        help="a list of genes that must be present in an entry for any gene "
-                             "sequences to be output",
-                        choices=knowngenes)
+                        help="the path to write the output csv")
     parser.add_argument("-r", "--genetypes", type=str, metavar='TYPE', nargs='+',
                         help="one or more gene types to be output",
                         choices=knowntypes, default=knowntypes)
-    parser.add_argument("-f", "--filter", type=str, metavar='PATH',
-                        help="path to a file listing LOCUS names, one per line; if supplied, only "
-                             "these entries will be output")
-    parser.add_argument("-n", "--organism", action='store_true',
-                        help="use the organism field instead of the locus field for sequence "
-                             "headers in the output files")
-    parser.add_argument("-w", "--writeunknowns", action='store_true',
-                        help="write fastas for unidentifiable annotations anyway")
-    parser.add_argument("-k", "--keepframe", action='store_true',
-                        help="remove excess out-of-frame bases at the beginning of truncated "
-                             "annotations")
-    parser.add_argument("-p", "--presence", type=str, metavar='PATH',
-                        help="if desired, the path to a text file to which will be written a list "
-                             "of genes present for each input entry")
+    parser.add_argument("-m", "--lenerrormult", type=float, metavar='N', default = 5,
+                        help=" multiple of the length variation percentage below which to denote "
+                        "annotation as truncated")
     parser.add_argument("-s", "--showgenes", action='store_true',
                         help="print the known gene name variants")
 
     # Parse arguments
     args = parser.parse_args(arglist) if arglist else parser.parse_args()
     # Do some checking of the inputs
-    if args.mingenes < 0:
-        parser.error(f"{args.mingenes} is not a possible minimum number of genes")
 
     # If the arguments are all OK, output them
     return args
@@ -139,6 +209,9 @@ if __name__ == "__main__":
         # Get the gene name variants
         nameconvert, annotypes, namevariants = loadnamevariants()
 
+        # Get the length specification
+        specs = parse_specs(nameconvert)
+
         # Get the arguments
         args = getcliargs(None, nameconvert.keys(), annotypes)
         # args = getcliargs('-g /home/thomas/work/iBioGen_postdoc/MMGdatabase/newdata_2022-04-15_syrphid/testnewsequences.gb -o testextract -w -k -p testpresence.txt'.split(' '),  nameconvert.keys(), annotypes)  # Read from a string, good for testing
@@ -148,42 +221,24 @@ if __name__ == "__main__":
             loadnamevariants(report=True)
             exit()
 
-        # Make output directory if necessary
-        if not os.path.exists(args.output):
-            os.makedirs(args.output)
-
         # Set up for unrecognised genes
         unrecgenes = defaultdict(list)
 
-        # Set up gene presence handle
-        if args.presence:
-            args.presence = open(args.presence, 'w')
-
-        # Read filter list
-        filter = []
-        if args.filter:
-            with open(args.filter) as fh:
-                for line in fh:
-                    filter.append(line.rstrip())
-
-        # Work through genbank files
-        nrejected = 0
-        outfh = {}
         # *Create a DataFrame to store the results
         df = pd.DataFrame(
             columns=['NAME', 'SEQLENGTH', 'PRESENT GENES', 'PRESENT&COMPLETE GENES', 'ND2', 'COX1', 'COX2', 'ATP8',
                      'ATP6', 'COX3', 'ND3', 'ND5', 'ND4', 'ND4L', 'ND6',
                      'CYTB', 'ND1'])
+
+        # Work through genbank files
+        outfh = {}
+        
         for gbpath in args.genbank:
             # gbpath = args.genbank[0]
             for seqrecord in SeqIO.parse(gbpath, "genbank"):
                 # gb = SeqIO.parse(gbpath, "genbank")
                 # seqrecord = next(gb)
-                # If filtering, check if this entry should be used
-                if args.filter and seqrecord.name not in filter:
-                    nrejected += 1
-                    continue
-
+                
                 # Extract names
                 seqname = seqrecord.name
                 outname = seqname
@@ -195,17 +250,12 @@ if __name__ == "__main__":
 
                 # *Create a row to store the information
                 row = pd.Series()
-                row["NAME"]=outname
-                row['SEQLENGTH']=seqlength
-
-
-
-
+                row["NAME"] = outname
+                row['SEQLENGTH'] = seqlength
 
                 # *Set up containers
-                foundgenes = defaultdict(list)
-                prenottrunc=defaultdict(list)
-                preandtrunc=defaultdict(list)
+                prenottrunc = defaultdict(list)
+                preandtrunc = defaultdict(list)
 
                 # Work through features
 
@@ -225,77 +275,38 @@ if __name__ == "__main__":
                             stdname = name
                         else:
                             continue
-
-
-
-                    # Store sequence for writing
+                    
+                    # Get sequence
                     featsequence = feat.extract(seqrecord.seq)
-                    if args.keepframe and 'codon_start' in feat.qualifiers:
-                        featsequence = featsequence[(int(feat.qualifiers['codon_start'][0]) - 1):]
-                    foundgenes[stdname].append(featsequence)
 
+                    # Check if length is too short
+                    maxerr = specs[stdname]['length'] * (specs[stdname]['length']/100) * args.lenerrormult
+                    short = len(featsequence) < (specs[stdname]['length'] - maxerr)
+                    
                     # *store genes present and not truncated
-                    if isinstance(feat.location.start, BeforePosition) or isinstance(feat.location.end, AfterPosition):
+                    if isinstance(feat.location.start, BeforePosition) or isinstance(feat.location.end, AfterPosition) or short:
                         preandtrunc[stdname].append(featsequence)
                     else:
                         prenottrunc[stdname].append(featsequence)
 
                 # *write the presence to the row
-                row['PRESENT GENES'] = len(foundgenes)
+                row['PRESENT GENES'] = len(prenottrunc) + len(preandtrunc)
                 row['PRESENT&COMPLETE GENES'] = len(prenottrunc)
-                for i in prenottrunc:
+                for i in prenottrunc.keys():
                     if i in df.columns:
                         row[i] = "P"
-                for i in preandtrunc:
+                for i in preandtrunc.keys():
                     if i in df.columns:
                         row[i] = 'T'
                     # *Add the presence of genes in the current sequence to the dataframe将当前序列的基因存在情况添加到数据表中
                 df = df.append(row, ignore_index=True)
 
-                # Check to see if there are enough found features and all required genes are present,
-                # if so write sequences out
-                if len(foundgenes) >= args.mingenes:
-                    if not args.reqgenes or all([g in foundgenes for g in args.reqgenes]):
-                        for gene, seqs in foundgenes.items():
-                            # Check the number of sequences and warn if there are > 1
-                            if len(seqs) > 1:
-                                sys.stderr.write(f"Warning: entry {seqname} has multiple annotations "
-                                                 f"of the target type ({'/'.join(args.genetypes)}) "
-                                                 f"matching the standard name \"{gene}\"\n")
-                            # Check to see if there's already a file handle for this gene, if not open
-                            if gene not in outfh:
-                                outfh[gene] = open(os.path.join(args.output, f"{gene}.fasta"), 'w')
-                            # Write out sequences
-                            for seq in seqs:
-                                outfh[gene].write(f">{outname}\n{seq}\n")
-
-                        #if args.presence:
-                            #args.presence.write(f"{outname},{seqlength},{len(foundgenes)},{len(prenottrunc)},{','.join(foundgenes.keys())},{','.join(prenottrunc.keys())}\n")
-
-                    else:
-                        sys.stderr.write(f"Warning: entry {seqname} does not include all required "
-                                         f"genes, it will be skipped")
-                else:
-                    sys.stderr.write(f"Warning: entry {seqname} has {len(foundgenes)} known "
-                                     f"annotations with sufficient information, it will be skipped")
-
         # *Output the dataframe as a CSV file
         if args.presence:
-            df.to_csv('completeness.csv', index=False)
-
-        # Close file handles
-        for gene, fh in outfh.items():
-            fh.close()
-        if args.presence:
-            args.presence.close()
+            df.to_csv(args.output, index=False)
 
         # Report unrecognised genes
         if len(unrecgenes) > 0:
             sys.stderr.write(f"Warning: unrecognised genes present in the following entries.\n")
             for gene, entries in unrecgenes.items():
                 sys.stderr.write(f"\t{gene} - {', '.join(sorted(list(set(entries))))}\n")
-
-        # Report rejected entries
-        if nrejected > 0:
-            sys.stderr.write(f"Warning: {nrejected} entries in genbank-format file(s) rejected for "
-                             f"not matching filter list")
